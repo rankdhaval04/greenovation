55b86bd84c60fa718f54e31e46a5003437a0e97a
rankdhaval04 <rankdhaval98@gmail.com>
2020-06-29 11:34:29 +0530

    first

---
+++ lib/pages/loginPage.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+import 'package:greenovation/pages/homePage.dart';
+import 'package:greenovation/pages/signup.dart';
+import 'package:greenovation/pages/signupNgo.dart';
+
+class loginPage extends StatefulWidget {
+  @override
+  _loginPageState createState() => _loginPageState();
+}
+
+class _loginPageState extends State<loginPage> {
+
+  final _formKey = GlobalKey<FormState>();
+  final phonenoController = new TextEditingController();
+  final passwordController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    final phoneno = TextFormField(
+      obscureText: false,
+      controller: phonenoController,
+      validator: (String value){
+//        Pattern pattern =
+//            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+//        RegExp regex = new RegExp(pattern);
+        if (value.length != 0)
+          return 'Enter Valid phone no';
+        else
+          return null;
+      },
+      style: Theme.of(context).textTheme.body1,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Phone no",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final passsword = TextFormField(
+      obscureText: true,
+      controller: passwordController,
+      validator: (String value){
+        if (value.length < 6)
+          return 'password must be more then 6 digit';
+        else
+          return null;
+      },
+      style: Theme.of(context).textTheme.body1,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final forgotpassword = Text('Forgot password ?', style: Theme
+        .of(context)
+        .textTheme
+        .subhead
+        .copyWith(color: Colors.black54),);
+
+    final loginButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Colors.green,
+      child: MaterialButton(
+        minWidth: MediaQuery
+            .of(context)
+            .size
+            .width / 1.1 ,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: ()  {
+          if(_formKey.currentState.validate()){
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => homePage()),
+            );
+          }
+        },
+        child: Text("Login",
+            textAlign: TextAlign.center,
+            style: Theme
+                .of(context)
+                .textTheme
+                .title.copyWith(color: Colors.white)
+        ),
+      ),
+    );
+
+    final donthaveone = Text("Don't have one ?",
+        textAlign: TextAlign.center,
+        style: Theme
+            .of(context)
+            .textTheme
+            .subtitle.copyWith(color: Colors.black54)
+    );
+
+    final ngo = Text("You are NGO ?",
+        textAlign: TextAlign.center,
+        style: Theme
+            .of(context)
+            .textTheme
+            .subtitle.copyWith(color: Colors.black54)
+    );
+
+    final signup = Text("Sign Up",
+        textAlign: TextAlign.center,
+        style: Theme
+            .of(context)
+            .textTheme
+            .subtitle.copyWith(color: Colors.black54, fontWeight: FontWeight.bold)
+    );
+
+    final signupNGO = Text("Sign Up",
+        textAlign: TextAlign.center,
+        style: Theme
+            .of(context)
+            .textTheme
+            .subtitle.copyWith(color: Colors.black54, fontWeight: FontWeight.bold)
+    );
+
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage('https://images.unsplash.com/photo-1525124480298-565f20ea00ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: SingleChildScrollView(
+            child: Card(
+              elevation: 5.0,
+              child: Form(
+                key: _formKey,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                          width: 100,
+                          height: 100,
+                          child: CircleAvatar(backgroundImage: NetworkImage("https://images.unsplash.com/photo-1472722266948-a898ab5ff257?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=891&q=80",))),
+                      SizedBox(height: 10.0,),
+                      phoneno,
+                      SizedBox(height: 10.0,),
+                      passsword,
+                      SizedBox(height: 10.0,),
+                      Container(
+                        child: Align(
+                            alignment: Alignment.topRight,
+                            child: forgotpassword),
+                      ),
+                      SizedBox(height: 10.0,),
+                    loginButon,
+                      SizedBox(height: 10.0,),
+                      Container(
+                        child: Row(
+                          children: <Widget>[
+                            SizedBox(width: MediaQuery.of(context).size.width / 4,),
+                            donthaveone,
+                            SizedBox(width: 10.0,),
+                            GestureDetector(
+                                onTap: (){
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => signUp()),
+                                  );
+                                },
+                                child: signup)
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 10.0,),
+
+                      Container(
+                        child: Row(
+                          children: <Widget>[
+                            SizedBox(width: MediaQuery.of(context).size.width / 3.8,),
+                            GestureDetector(
+                                onTap: (){
+
+                                },
+                                child: ngo),
+                            SizedBox(width: 10.0,),
+                            GestureDetector(
+                                onTap: (){
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => signupNgo()),
+                                  );
+                                },
+                                child: signupNGO)
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 10.0,)
+                ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/signupNgo.dart
@@ -0,0 +1,258 @@
+import 'package:flutter/material.dart';
+import 'package:greenovation/pages/loginPage.dart';
+
+
+class signupNgo extends StatefulWidget {
+  @override
+  _signupNgoState createState() => _signupNgoState();
+}
+
+class _signupNgoState extends State<signupNgo> {
+
+  final _formKey = GlobalKey<FormState>();
+  final nameController = new TextEditingController();
+  final phoneController = new TextEditingController();
+  final emailController = new TextEditingController();
+  final passwordController = new TextEditingController();
+  final dobController = new TextEditingController();
+  final cityController = new TextEditingController();
+  final stateController = new TextEditingController();
+  final pincodeController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    final name = TextFormField(
+      controller: nameController,
+      validator: (String value) {
+        if (value.length == 0)
+          return 'Enter name';
+        else
+          return null;
+      },
+      style: Theme
+          .of(context)
+          .textTheme
+          .body1,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "NGO Name",
+          border:
+          OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final phone = TextFormField(
+      controller: phoneController,
+      keyboardType: TextInputType.number,
+      validator: (String value) {
+        if (value.length < 10 || value.length > 10)
+          return 'Enter mobile no';
+        else
+          return null;
+      },
+      style: Theme
+          .of(context)
+          .textTheme
+          .body1,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "NGO Mobile no",
+          border:
+          OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final email = TextFormField(
+      obscureText: false,
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      validator: (String value) {
+        Pattern pattern =
+            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+        RegExp regex = new RegExp(pattern);
+        if (!regex.hasMatch(value))
+          return 'Enter Valid Email';
+        else
+          return null;
+      },
+      style: Theme
+          .of(context)
+          .textTheme
+          .body1,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "NGO Email",
+          border:
+          OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final passsword = TextFormField(
+      obscureText: true,
+      controller: passwordController,
+      validator: (String value){
+        if (value.length < 6)
+          return 'password must be more then 6 digit';
+        else
+          return null;
+      },
+      style: Theme.of(context).textTheme.body1,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final dob = TextFormField(
+      controller: dobController,
+      keyboardType: TextInputType.datetime,
+      validator: (String value) {
+        if (value.length < 5)
+          return 'Enter valid date ';
+        else
+          return null;
+      },
+      style: Theme
+          .of(context)
+          .textTheme
+          .body1,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "NGO Foundation Date",
+          border:
+          OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final city = TextFormField(
+      controller: cityController,
+      validator: (String value) {
+        if (value.length < 3)
+          return 'Enter valid city';
+        else
+          return null;
+      },
+      style: Theme
+          .of(context)
+          .textTheme
+          .body1,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "NGO City",
+          border:
+          OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final pincode = TextFormField(
+      controller: pincodeController,
+      keyboardType: TextInputType.number,
+      validator: (String value) {
+        if (value.length < 6)
+          return 'Enter valid pincode';
+        else
+          return null;
+      },
+      style: Theme
+          .of(context)
+          .textTheme
+          .body1,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "pincode",
+          border:
+          OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final state = TextFormField(
+      controller: stateController,
+      validator: (String value) {
+        if (value.length < 3)
+          return 'Enter valid state';
+        else
+          return null;
+      },
+      style: Theme
+          .of(context)
+          .textTheme
+          .body1,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "State",
+          border:
+          OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final button = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Colors.teal,
+      child: MaterialButton(
+        minWidth: MediaQuery
+            .of(context)
+            .size
+            .width / 1.1,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () async {
+          if (_formKey.currentState.validate()) {
+
+            _formKey.currentState.save();
+
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => loginPage()),
+            );
+          }
+        },
+        child: Text("Register NGO",
+            textAlign: TextAlign.center,
+            style: Theme
+                .of(context)
+                .textTheme
+                .title
+                .copyWith(color: Colors.white)
+        ),
+      ),
+    );
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              SizedBox(height: 20.0,),
+              Align(
+                  alignment: Alignment.center,
+                  child: Text('Register NGO',style: Theme.of(context).textTheme.headline.copyWith(color: Colors.teal),)),
+              SizedBox(height: 10.0,),
+              name,
+              SizedBox(height: 10.0,),
+              email,
+              SizedBox(height: 10.0,),
+              phone,
+              SizedBox(height: 10.0,),
+              passsword,
+              SizedBox(height: 10.0,),
+              dob,
+              SizedBox(height: 10.0,),
+              city,
+              SizedBox(height: 10.0,),
+              state,
+              SizedBox(height: 10.0,),
+              pincode,
+              SizedBox(height: 10.0,),
+              button,
+              SizedBox(height: 10.0,),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
